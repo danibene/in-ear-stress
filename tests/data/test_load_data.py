a1c8b49ba875bbffc56301ee69c63659f8dfa1ab
danibene <34680344+danibene@users.noreply.github.com>
2024-07-18 12:16:47 -0400

    add docstrings and more meaningful assert

--- tests/data/test_load_data.py
+++ tests/data/test_load_data.py
@@ -7,6 +7,23 @@ from stresspred.data.load_data import StressBioDataLoader
 
 class TestStressBioDataLoader:
     def create_ibi_df(self):
+        """
+        Creates a DataFrame containing Inter-Beat Intervals (IBIs).
+
+        Uses a given set of signals, participants, tasks, and rest states.
+
+        Returns
+        -------
+        pandas.DataFrame
+            A DataFrame with the following columns:
+                - "Signal" (str): The name of the signal.
+                - "Participant" (str): The name of the participant.
+                - "Task" (str): The name of the task.
+                - "Rest" (str): The rest state (either "Rest" or "Stress").
+                - "SubSegIdx" (int): The sub-segment index.
+                - "Ibi" (float): The Inter-Beat Interval in seconds.
+                - "IbiTime" (float): The time of the Inter-Beat Interval in seconds.
+        """
         signals = ["ECG"]
         participants = ["Test"]
         tasks = ["MENTAL", "NOISE", "CPT"]
@@ -46,9 +63,12 @@ class TestStressBioDataLoader:
         return ibi_df
 
     def test_get_base_feat_df(self):
+        """Test function for getting the baseline feature DataFrame."""
         ibi_df = self.create_ibi_df()
-        assert ibi_df["Rest"].iloc[0] == "Rest"
         data_loader = StressBioDataLoader()
-        data_loader.get_base_feat_df(
+        base_feat_df = data_loader.get_base_feat_df(
             load_from_file=False, in_data=ibi_df, save_file=False
         )
+        index_cols = ["Signal", "Participant", "Task", "Rest", "SubSegIdx"]
+        n_unique_segments = ibi_df[index_cols].drop_duplicates().shape[0]
+        assert base_feat_df.shape[0] == n_unique_segments
