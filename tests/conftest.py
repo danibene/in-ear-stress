2e7e377751323e2b5c499ac2f1313ad65e75c2d6
danibene <34680344+danibene@users.noreply.github.com>
2024-07-28 13:55:39 -0400

    add tests using common dataframe

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,94 @@
     - https://docs.pytest.org/en/stable/writing_plugins.html
 """
 
-# import pytest
+from pathlib import Path
+
+import neurokit2 as nk
+import pandas as pd
+import pytest
+from tempbeat.extraction.interval_conversion import peak_time_to_rri
+
+
+class Helpers:
+    @staticmethod
+    def help_do_things():
+        pass
+
+
+@pytest.fixture
+def helpers():
+    return Helpers
+
+
+def create_ibi_df():
+    """
+    Creates a DataFrame containing Inter-Beat Intervals (IBIs).
+
+    Uses a given set of signals, participants, tasks, and rest states.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame with the following columns:
+            - "Signal" (str): The name of the signal.
+            - "Participant" (str): The name of the participant.
+            - "Task" (str): The name of the task.
+            - "Rest" (str): The rest state (either "Rest" or "Stress").
+            - "SubSegIdx" (int): The sub-segment index.
+            - "Ibi" (float): The Inter-Beat Interval in seconds.
+            - "IbiTime" (float): The time of the Inter-Beat Interval in seconds.
+    """
+    signals = ["ECG"]
+    participants = ["Test1", "Test2", "Test3"]
+    tasks = ["MENTAL", "NOISE", "CPT"]
+    rest = ["Rest", "Stress"]
+    sub_seg_idxs = [1]
+    duration = 180
+    seg_dfs = []
+    random_state = 0
+    for signal in signals:
+        for participant in participants:
+            for task in tasks:
+                for rest_val in rest:
+                    for sub_seg_idx in sub_seg_idxs:
+                        random_state += 1
+                        ecg = nk.ecg_simulate(
+                            duration=duration, random_state=random_state
+                        )
+                        signals, info = nk.ecg_process(ecg)
+                        ecg = nk.ecg_simulate(duration=duration)
+                        _, info = nk.ecg_process(ecg)
+                        rri, rri_time = peak_time_to_rri(
+                            info["ECG_R_Peaks"] / info["sampling_rate"]
+                        )
+                        seg_dict = {
+                            "Signal": [signal] * len(rri),
+                            "Participant": [participant] * len(rri),
+                            "Task": [task] * len(rri),
+                            "Rest": [rest_val] * len(rri),
+                            "SubSegIdx": [sub_seg_idx] * len(rri),
+                            "Ibi": rri,
+                            "IbiTime": rri_time,
+                        }
+                        seg_df = pd.DataFrame.from_dict(seg_dict)
+                        seg_dfs.append(seg_df)
+
+    ibi_df = pd.concat(seg_dfs)
+    return ibi_df
+
+
+@pytest.fixture(scope="session")
+def example_data_paths(tmpdir_factory):
+    data_dir = tmpdir_factory.mktemp("data")
+    derivatives_dir = Path(data_dir, "derivatives")
+    Path(derivatives_dir).mkdir(exist_ok=True)
+    stress_class_dir = Path(derivatives_dir, "stress_class")
+    Path(stress_class_dir).mkdir(exist_ok=True)
+    ibi_df = create_ibi_df()
+    ibi_df_path = Path(stress_class_dir, "stress_class_arp_ibi_20220210_fs_500.csv")
+    ibi_df.to_csv(str(ibi_df_path), index=False)
+    example_data_paths_dict = {
+        "data_dir": data_dir,
+        "ibi_df": ibi_df_path,
+    }
+    return example_data_paths_dict
--- tests/data/test_load_data.py
+++ tests/data/test_load_data.py
@@ -1,70 +1,13 @@
-import neurokit2 as nk
 import pandas as pd
-from tempbeat.extraction.interval_conversion import peak_time_to_rri
 
-from stresspred.data.load_data import StressBioDataLoader
+from stresspred.data.load_data import AudaceDataLoader, StressBioDataLoader
 
 
 class TestStressBioDataLoader:
-    def create_ibi_df(self):
-        """
-        Creates a DataFrame containing Inter-Beat Intervals (IBIs).
-
-        Uses a given set of signals, participants, tasks, and rest states.
-
-        Returns
-        -------
-        pandas.DataFrame
-            A DataFrame with the following columns:
-                - "Signal" (str): The name of the signal.
-                - "Participant" (str): The name of the participant.
-                - "Task" (str): The name of the task.
-                - "Rest" (str): The rest state (either "Rest" or "Stress").
-                - "SubSegIdx" (int): The sub-segment index.
-                - "Ibi" (float): The Inter-Beat Interval in seconds.
-                - "IbiTime" (float): The time of the Inter-Beat Interval in seconds.
-        """
-        signals = ["ECG"]
-        participants = ["Test"]
-        tasks = ["MENTAL", "NOISE", "CPT"]
-        rest = ["Rest", "Stress"]
-        sub_seg_idxs = [1]
-        duration = 180
-        seg_dfs = []
-        random_state = 0
-        for signal in signals:
-            for participant in participants:
-                for task in tasks:
-                    for rest_val in rest:
-                        for sub_seg_idx in sub_seg_idxs:
-                            random_state += 1
-                            ecg = nk.ecg_simulate(
-                                duration=duration, random_state=random_state
-                            )
-                            signals, info = nk.ecg_process(ecg)
-                            ecg = nk.ecg_simulate(duration=duration)
-                            _, info = nk.ecg_process(ecg)
-                            rri, rri_time = peak_time_to_rri(
-                                info["ECG_R_Peaks"] / info["sampling_rate"]
-                            )
-                            seg_dict = {
-                                "Signal": [signal] * len(rri),
-                                "Participant": [participant] * len(rri),
-                                "Task": [task] * len(rri),
-                                "Rest": [rest_val] * len(rri),
-                                "SubSegIdx": [sub_seg_idx] * len(rri),
-                                "Ibi": rri,
-                                "IbiTime": rri_time,
-                            }
-                            seg_df = pd.DataFrame.from_dict(seg_dict)
-                            seg_dfs.append(seg_df)
-
-        ibi_df = pd.concat(seg_dfs)
-        return ibi_df
-
-    def test_get_base_feat_df(self):
+    @staticmethod
+    def test_get_base_feat_df(example_data_paths):
         """Test function for getting the baseline feature DataFrame."""
-        ibi_df = self.create_ibi_df()
+        ibi_df = pd.read_csv(str(example_data_paths["ibi_df"]))
         data_loader = StressBioDataLoader()
         base_feat_df = data_loader.get_base_feat_df(
             load_from_file=False, in_data=ibi_df, save_file=False
@@ -72,3 +15,46 @@ class TestStressBioDataLoader:
         index_cols = ["Signal", "Participant", "Task", "Rest", "SubSegIdx"]
         n_unique_segments = ibi_df[index_cols].drop_duplicates().shape[0]
         assert base_feat_df.shape[0] == n_unique_segments
+
+
+class TestAudaceDataLoader:
+
+    @staticmethod
+    def test_get_ibi_df(example_data_paths):
+        train_loader = AudaceDataLoader(root=example_data_paths["data_dir"])
+        in_data = train_loader.get_ibi_df()
+
+        assert in_data is not None
+
+    @staticmethod
+    def test_get_split_pred_df(example_data_paths):
+        train_loader = AudaceDataLoader(root=example_data_paths["data_dir"])
+
+        in_data = train_loader.get_ibi_df()
+
+        # Use original feature values
+        # rather than those relative to each participant's baseline
+        rel_values = False
+        # Do not use spreadsheet with already extracted features
+        load_from_file = False
+        # Save spreadsheet with features
+        save_file = True
+
+        # Load "out_train" dictionary containing:
+        # X: all possible input features
+        # y: ground truth stress/rest labels
+        # method: method used for heartbeat extraction
+        # signal: type of heartbeat signal (ECG, IEML, etc.)
+        # sub: participant labels
+        # task: task labels (rest periods are labeled with the task that came after)
+        # subseg_data: if there are multiple segments corresponding to the same
+        # set of labels (e.g. with smaller segment lengths or error augmentation),
+        # their indices
+        out_train = train_loader.get_split_pred_df(
+            load_from_file=load_from_file,
+            save_file=save_file,
+            rel_values=rel_values,
+            in_data=in_data,
+        )
+
+        assert out_train is not None
