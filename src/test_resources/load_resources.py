4ef7c1afdbe188ac9beb346fe45dcfd814cfab67
danibene <34680344+danibene@users.noreply.github.com>
2024-07-28 21:44:51 -0400

    use with statement in get_resource_path

--- src/test_resources/load_resources.py
+++ src/test_resources/load_resources.py
@@ -5,5 +5,5 @@ import test_resources
 
 
 def get_resource_path(file_name):
-    root_path = pkg_resources.path(test_resources, "files")
-    return Path(root_path, file_name)
+    with pkg_resources.path(test_resources, "files") as root_path:
+        return Path(root_path) / file_name
