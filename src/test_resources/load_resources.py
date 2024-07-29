7fd6ed44b7de268aa07814a9a15a0d7f6a15b770
danibene <34680344+danibene@users.noreply.github.com>
2024-07-28 20:52:31 -0400

    move location of simulated ibi df

--- src/test_resources/load_resources.py
+++ src/test_resources/load_resources.py
@@ -1,7 +1,9 @@
 import importlib.resources as pkg_resources
+from pathlib import Path
 
 import test_resources
 
 
 def get_resource_path(file_name):
-    return pkg_resources.path(test_resources, file_name)
+    root_path = pkg_resources.path(test_resources, "files")
+    return Path(root_path, file_name)
